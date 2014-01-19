f744a856c5eb869efdcb5150fd88c32ca223200f
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-01-19 14:37:22 +0100

    Fixed a typo in SassyJSON.rb

--- lib/SassyJSON.rb
+++ lib/SassyJSON.rb
@@ -4,7 +4,7 @@ Compass::Frameworks.register('SassyJSON', :path => extension_path)
 
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
-module SassyLists
+module SassyJSON
   VERSION = "1.0.6"
   DATE = "2014-01-19"
 end
