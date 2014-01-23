d0647663d62f5e6eb4f4480e81688df4abae6a49
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-01-23 16:24:25 +0100

    Bump 1.0.10

--- lib/SassyJSON.rb
+++ lib/SassyJSON.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('SassyJSON', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyJSON
-  VERSION = "1.0.9"
-  DATE = "2014-01-21"
+  VERSION = "1.0.10"
+  DATE = "2014-01-23"
 end
 
 module Sass::Script::Functions
