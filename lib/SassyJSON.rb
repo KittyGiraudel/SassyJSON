61dbde78293145f4b8a3c88c49f435ac1ad9b84b
Fabrice Weinberg <Fabrice@weinberg.me>
2014-01-30 20:01:52 +0100

    Add JsonImporter!

--- lib/SassyJSON.rb
+++ lib/SassyJSON.rb
@@ -1,14 +1,12 @@
 require 'compass'
+require 'JsonImporter'
+
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('SassyJSON', :path => extension_path)
 
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyJSON
-  VERSION = "1.1.1"
+  VERSION = "1.1.2"
   DATE = "2014-01-29"
-end
-
-module Sass::Script::Functions
-
 end
